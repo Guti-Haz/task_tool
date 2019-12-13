fe42c349f2bd46a47cc72111b777e7cbb27e11bf
Guti-Haz <30297859+Guti-Haz@users.noreply.github.com>
2019-12-13 09:23:16 +0800

    Update initiateDB.R

--- initiateDB.R
+++ initiateDB.R
@@ -1,13 +1,11 @@
 pacman::p_load(RSQLite,DBI,rstudioapi,glue,magrittr,data.table)
 setwd(dirname(getSourceEditorContext()$path))
-# create db ----
+# create db
 if("db.sqlite"%in%list.files()==T) unlink("db.sqlite")
 db=dbConnect(RSQLite::SQLite(),"db.sqlite")
-# create table ----
+# create table
 tableName="timeline"
 colName=c("project",
-          "importance",
-          "sub_project",
           "task",
           "task_dependency",
           "duration_week",
@@ -16,22 +14,11 @@ colName=c("project",
 colType=c("TEXT",
           "TEXT",
           "TEXT",
-          "TEXT",
-          "TEXT",
           "INTEGER",
           "TEXT",
           "TEXT")
 colSql=paste0(colName," ",colType)%>%paste0(collapse=", ")
 q=glue("CREATE TABLE {tableName} ({colSql})")
 dbExecute(db,q)
-# # add rows ----
-# testData=data.table(project="STR prioritization",
-#              importance="high",
-#              sub_project="EOI",
-#              task="scrap",
-#              duration_week=10,
-#              startDate="2019-01-01",
-#              endDate="2020-01-01")
-# dbAppendTable(db,tableName,testData)
-# disconnect ----
-
+# disconnect
+dbDisconnect(db)
